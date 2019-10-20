d4d0796e06945034b61b3e2deb854820aafe7d44
rhofp <rhodfra@gmail.com>
2019-10-20 05:04:10 -0500

    Inicio vistas de admin

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/admin/cliente/mostrarTodos','ClienteController@showAll');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
